69157108bc413eb53d3ed1c577aba3d0d3d3c2f7
Morgan Chang <shin19991207@gmail.com>
2025-06-08 18:11:01 -0400

    update module1 deployapp script

--- module1/deployModResorts.py
+++ module1/deployModResorts.py
@@ -1,14 +1,11 @@
-#!/bin/sh
-
-# Variables Section
-# Location of files for Demo
 USERHOME = "/home/techzone"
-DEMOPATH = USERHOME + "/Student/tx-more-lab/module1"
+DEMOPATH = USERHOME + "/Student/tx-more-lab"
+DMGRCONFIGPATH = USERHOME + "/IBM/WebSphere/AppServer/profiles/Dmgr01/config"
 
 # Install Application ModResorts
-print("***Now installing Application Mod Resorts***")
+print "***Now installing Application Mod Resorts***"
 AdminApp.install(
-    DEMOPATH + "/modresorts/target/modresorts-2.0.0.war",
+    DEMOPATH + "/module1/modresorts/target/modresorts-2.0.0.war",
     "[ -distributeApp -useMetaDataFromBinary -appname modresorts-2_0_0_war -validateinstall warn "
     "-noallowDispatchRemoteInclude -noallowServiceRemoteInclude -novalidateSchema -contextroot /resorts "
     "-MapModulesToServers [[ modresorts-2.0.0.war modresorts-2.0.0.war,WEB-INF/web.xml WebSphere:cell=MoREDemoCell,cluster=MLSCluster ] "
@@ -17,17 +14,18 @@ AdminApp.install(
 AdminConfig.save()
 
 # Generate and Propogate Plugin
-CONFIGPATH = USERHOME + "/IBM/WebSphere/AppServer/profiles/Dmgr01/config"
-
+print "***Generating Plugin for ModResorts***"
 AdminControl.invoke(
     'WebSphere:name=PluginCfgGenerator,process=dmgr,platform=common,node=CellManager,version=9.0.5.24,type=PluginCfgGenerator,mbeanIdentifier=PluginCfgGenerator,cell=MoREDemoCell,spec=1.0',
     'generate',
-    [CONFIGPATH, "MoREDemoCell", "node2", "webserver1", "false"]
+    [DMGRCONFIGPATH, "MoREDemoCell", "node2", "webserver1", "false"]
 )
 
-print("***Propogating Plugin***")
+print "***Propogating Plugin for ModResorts***"
 AdminControl.invoke(
     'WebSphere:name=PluginCfgGenerator,process=dmgr,platform=common,node=CellManager,version=9.0.5.24,type=PluginCfgGenerator,mbeanIdentifier=PluginCfgGenerator,cell=MoREDemoCell,spec=1.0',
     'propagate',
-    [CONFIGPATH, "MoREDemoCell", "node2", "webserver1"]
+    [DMGRCONFIGPATH, "MoREDemoCell", "node2", "webserver1"]
 )
+
+print "ModResorts deployed. Visit: http://<hostname>:7777/resorts"
