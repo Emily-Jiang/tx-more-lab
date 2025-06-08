34dfc82341fa91d5055f4a6c2903f3e7f7d8f840
Morgan Chang <shin19991207@gmail.com>
2025-06-08 17:54:38 -0400

    refactor createMLSCluster.py

--- createMLSCluster.py
+++ createMLSCluster.py
@@ -1,21 +1,17 @@
-#!/bin/sh
-
 # Create MLS Cluster
-print("***Creating cluster MLSCluster***")
+print "***Creating cluster MLSCluster***"
 AdminTask.createCluster(['-clusterConfig', ['-clusterName', 'MLSCluster', '-clusterType', 'MANAGED_LIBERTY_SERVER']])
-print("***Finished createCluster***")
+print "***Creating createClusterMember for node1***"
 AdminTask.createClusterMember(['-clusterName', 'MLSCluster', '-memberConfig', ['-memberNode', 'node1', '-memberName', 'libertyServer']])
-print("***Finished createClusterMember for node1***")
+print "***Creating createClusterMember for node2***"
 AdminTask.createClusterMember(['-clusterName', 'MLSCluster', '-memberConfig', ['-memberNode', 'node2', '-memberName', 'libertyServer']])
-print("***Finished createClusterMember for node2***")
-print("***Saving***")
 AdminConfig.save()
-print("***Syncing Node***")
+
+print "***Syncing nodes***"
 AdminNodeManagement.syncActiveNodes()
-print("***Successfully synced the nodes***")
 
 # Start the MLS Cluster
-print("***Now starting the cluster MLSCluster***")
+print "***Starting the cluster MLSCluster***"
 cluster = AdminControl.queryNames('WebSphere:*,type=Cluster,name=MLSCluster')
 AdminControl.invoke(cluster, 'start')
-print("!!!Successfully started the cluster!!!")
+print "!!!Successfully started the cluster!!!"
