e9de4d021bed604c4532190a076ab7a2e05b04c9
Morgan Chang <shin19991207@gmail.com>
2025-06-08 17:54:49 -0400

    Create createWebServer.py

---
+++ createWebServer.py
@@ -0,0 +1,38 @@
+USERHOME = "/home/techzone"
+
+# Create Web Server on a Managed Node
+print "***Creating Web Server webserver1 on node2***"
+AdminTask.createWebServer('node2', f'''[
+    -name webserver1 
+    -templateName IHS 
+    -serverConfig [
+        -webPort 7777 
+        -serviceName 
+        -webInstallRoot {USERHOME}/IBM/HTTPServer 
+        -webProtocol HTTP 
+        -configurationFile 
+        -errorLogfile 
+        -accessLogfile 
+        -pluginInstallRoot {USERHOME}/IBM/HTTPServer/plugin 
+        -webAppMapping ALL
+    ] 
+    -remoteServerConfig [
+        -adminPort 8008 
+        -adminUserID 
+        -adminPasswd ******** 
+        HTTP
+    ]
+]''')
+AdminConfig.save()
+
+print "***Creating the host alias***"
+AdminConfig.create(
+    'HostAlias',
+    AdminConfig.getid('/Cell:MoREDemoCell/VirtualHost:default_host/'),
+    '[[hostname "*"] [port "7777"]]'
+)
+
+# Start Web Server
+print "***Starting Web Server***"
+AdminTask.startMiddlewareServer('[-serverName webserver1 -nodeName node2 ]')
+print "IHS is running at http://<hostname>:7777"
