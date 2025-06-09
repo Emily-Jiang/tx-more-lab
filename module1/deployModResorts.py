5045697e6ffb7432d453de9f897330b0dd16c5e9
Morgan Chang <shin19991207@gmail.com>
2025-06-09 00:35:29 -0400

    Update deployModResorts.py

--- module1/deployModResorts.py
+++ module1/deployModResorts.py
@@ -8,7 +8,7 @@ AdminApp.install(
     DEMOPATH + "/module1/modresorts/target/modresorts-2.0.0.war",
     "[ -distributeApp -useMetaDataFromBinary -appname modresorts-2_0_0_war -validateinstall warn "
     "-noallowDispatchRemoteInclude -noallowServiceRemoteInclude -novalidateSchema -contextroot /resorts "
-    "-MapModulesToServers [[ modresorts-2.0.0.war modresorts-2.0.0.war,WEB-INF/web.xml WebSphere:cell=MoREDemoCell,cluster=MLSCluster ]] ]"
+    "-MapModulesToServers [[ modresorts-2.0.0.war modresorts-2.0.0.war,WEB-INF/web.xml WebSphere:cell=MoREDemoCell,cluster=MLSCluster+WebSphere:cell=MoREDemoCell,node=node2,server=webserver1 ]] ]"
 )
 AdminConfig.save()
 
